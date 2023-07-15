e123001411ef997fcba9ffe089662f31415a9fb9
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2023-07-14 23:17:29 -0700

    Add Unit Test Projects

---
+++ src/Client.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Client.UnitTests;
+
+public class UnitTest1
+{
+	[Fact]
+	public void Test1()
+	{
+
+	}
+}
---
+++ src/Shared.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Shared.UnitTests;
+
+public class UnitTest1
+{
+	[Fact]
+	public void Test1()
+	{
+
+	}
+}
