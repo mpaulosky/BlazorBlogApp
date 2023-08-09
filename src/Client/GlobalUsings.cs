0a7d46480144b717c828b0e5bd84d3f73b03b6af
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2023-08-09 10:24:18 -0700

    Cleanup and Refactor files

---
+++ src/Client/GlobalUsings.cs
@@ -0,0 +1,18 @@
+// ============================================
+// Copyright (c) 2023. All rights reserved.
+// File Name :     GlobalUsings.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : BlazorBlogApp
+// Project Name :  BlazorBlog.Client
+// =============================================
+
+global using BlazorBlog.Client;
+global using BlazorBlog.Client.Services;
+global using BlazorBlog.Shared;
+
+global using Flurl.Http;
+global using Flurl.Http.Configuration;
+
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -7,17 +7,16 @@
 // Project Name :  BlazorBlog.Client
 // =============================================
 
-using BlazorBlog.Client;
-using BlazorBlog.Client.Services;
-
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Flurl;
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new Url(builder.HostEnvironment.BaseAddress));
+builder.Services.AddScoped<IFlurlClientFactory, PerBaseUrlFlurlClientFactory>();
+
 builder.Services.AddScoped<IBlogService, BlogService>();
 
 await builder.Build().RunAsync();
